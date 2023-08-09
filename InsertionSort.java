884994245cfd8ed4dc8be91e180b1bbaaf2fbc40
yegneswar-rayavaram <yegneswar.rayavaram@gmail.com>
2023-08-09 14:29:37 +0530

    added code for insertion sort using java

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        InsertionSortArray(n, arr);
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
+
+    public static void InsertionSortArray(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
